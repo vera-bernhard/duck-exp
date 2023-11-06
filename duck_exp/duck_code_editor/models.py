d9eef25f5a15ab6bc71f6e965e7cce3786a647f1
veralara.bernhard <veralara.bernhard@uzh.ch>
2023-11-06 13:35:19 +0100

    Enable some local code compilation

--- duck_exp/duck_code_editor/models.py
+++ duck_exp/duck_code_editor/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class CodeSnippet(models.Model):
     title = models.CharField(max_length=100)
     code = models.TextField()
--- duck_exp/duck_code_editor/views.py
+++ duck_exp/duck_code_editor/views.py
@@ -1,19 +1,32 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 # duck_code_editor/views.py
+import subprocess
 from django.shortcuts import render, redirect
 from .forms import CodeSnippetForm
 
 def code_editor(request):
+    output = None
+
     if request.method == 'POST':
         form = CodeSnippetForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('duck_code_editor:code_editor')
+            code_snippet = form.save()
+
+            # Execute the code using subprocess
+            try:
+                output = execute_code(code_snippet.code)
+            except Exception as e:
+                output = f"Error: {e}"
+
     else:
         form = CodeSnippetForm()
 
-    return render(request, 'duck_code_editor/code_editor.html', {'form': form})
+    return render(request, 'duck_code_editor/code_editor.html', {'form': form, 'output': output})
 
+def execute_code(code):
+    try:
+        # Use subprocess to run the code in a separate process
+        result = subprocess.check_output(["python", "-c", code], stderr=subprocess.STDOUT, text=True)
+        return result.strip()
+    except subprocess.CalledProcessError as e:
+        # Handle errors, if any
+        return f"Error: {e.output.strip()}"
