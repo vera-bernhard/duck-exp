4e48602d18adbe5876f91c8c959dd01e417a54f5
veralara.bernhard <veralara.bernhard@uzh.ch>
2023-11-19 23:00:39 +0100

    Add all pages

--- duck_exp/duck_code_editor/urls.py
+++ duck_exp/duck_code_editor/urls.py
@@ -1,11 +1,18 @@
-# duck_code_editor/urls.py
 from django.urls import path
-from .views import code_editor, survey_complete, start_experiment
+from .views import code_editor, code_editor_duck, survey_complete, start_experiment, instructions, feedback, survey, rubber_duck_instructions, break_page
 
 app_name = 'duck_code_editor'
 
 urlpatterns = [
-    path('code-editor/<int:task_number>/', code_editor, name='code_editor'),
+    path('', start_experiment, name='start_experiment'),
+    path('task<int:task_number>/', code_editor, name='code_editor'),
+    path('task<int:task_number>duck/', code_editor_duck, name='code_editor_duck'),
+    path('task<int:task_number>/feedback/', feedback, name='feedback'),
+    path('task<int:task_number>duck/feedback/', feedback, name='feedback_duck'),
     path('survey-complete/', survey_complete, name='survey_complete'),
-    path('', start_experiment, name='start_experiment'),  # Make start_experiment the default view
+    path('instructions/', instructions, name='instructions'),
+    path('instructions-duck/', rubber_duck_instructions,
+         name='rubber_duck_instructions'),
+    path('break/', break_page, name='break_page'),
+    path('survey', survey, name='survey')
 ]
--- duck_exp/duck_code_editor/views.py
+++ duck_exp/duck_code_editor/views.py
@@ -1,47 +1,113 @@
 # duck_code_editor/views.py
 import subprocess
 import json
+import regex as re
 from django.shortcuts import render, redirect
 from django.http import HttpResponseBadRequest
 from .forms import CodeSnippetForm
 
+
 def code_editor(request, task_number):
     # Retrieve the study_id from the session
     selected_study_id = request.session.get('selected_study_id')
-    # ...
-    
+
     print(f'study_id: {selected_study_id}, task_number: {task_number}')
-    
+
     with open('tasks/task_assignment.json', 'r') as assignment_file:
         study_assignment = json.load(assignment_file)
 
     # Get the task set associated with the study ID, defaulting to 'default_task_set' if not found
-    task_set_name = study_assignment.get(selected_study_id, 'task_set_a')
-    task_set_a = load_tasks(f'tasks/{task_set_name}.json')
+    task_set_name = study_assignment.get(selected_study_id)
+    task_test = load_tasks(f'tasks/{task_set_name}.json')
 
     # Validate that the task_number is a non-negative integer
     try:
         task_number = int(task_number)
-        if task_number < 0 or task_number >= len(task_set_a):
+        if task_number < 0 or task_number >= len(task_test) + 1:
             raise ValueError("Invalid task number")
     except ValueError:
         return HttpResponseBadRequest("Invalid task number")
 
-    current_task = task_set_a[task_number]
     output = None
-    
+    try:
+        current_task = task_test[task_number]
+    except IndexError:
+        return redirect('duck_code_editor:break_page')
+
     if request.method == 'POST':
         # Check if 'Next Task' button is pressed
-        if 'next_task' in request.POST:
-            # If 'Next Task' button is pressed, increment the task number
-            next_task_number = task_number + 1
+        if 'next' in request.POST:
+            # Redirect to the next task with its number in the URL
+            return redirect('duck_code_editor:feedback', task_number=task_number)
 
-            # If we've reached the end of tasks, redirect to the survey completion page
-            if next_task_number >= len(task_set_a):
-                return redirect('duck_code_editor:survey_complete')  # Use the correct URL name
+        elif 'run_code' in request.POST:
+
+            print('run_code')
+            # If 'Run Code' button is pressed, execute the code
+            form = CodeSnippetForm(request.POST)
+            if form.is_valid():
+                code_snippet = form.save()
+                try:
+                    output = execute_code(code_snippet.code)
+                except Exception as e:
+                    output = f"Error: {e}"
+            else:
+                output = None
+    else:
+        # Initialize the form with the code from the current task
+        form = CodeSnippetForm(
+            initial={'code': current_task.get('code_to_debug', '')})
+
+    return render(request, 'duck_code_editor/code_editor.html', {
+        'form': form,
+        'task_number': task_number+1,
+        'output': output,
+        'current_task': current_task,
+        'duck': False,
+    })
+
+# The rest of your code remains unchanged
+
+
+def code_editor_duck(request, task_number):
+    # Retrieve the study_id from the session
+    selected_study_id = request.session.get('selected_study_id')
+    print(f'study_id: {selected_study_id}, task_number: {task_number}')
+
+    with open('tasks/task_assignment.json', 'r') as assignment_file:
+        study_assignment = json.load(assignment_file)
+
+    # Get the task set associated with the study ID, defaulting to 'default_task_set' if not found
+    task_set_name = study_assignment.get(selected_study_id)
+    # take the other task set
+    if task_set_name == 'task_set_a':
+        task_set_name = 'task_set_b'
+    else:
+        task_set_name = 'task_set_a'
+    task_set = load_tasks(f'tasks/{task_set_name}.json')
+
+    # Validate that the task_number is a non-negative integer
+    try:
+        task_number = int(task_number)
+        if task_number < 0 or task_number >= len(task_set) + 1:
+            raise ValueError("Invalid task number")
+    except ValueError:
+        return HttpResponseBadRequest("Invalid task number")
+
+    try:
+        current_task = task_set[task_number]
+    except IndexError:
+        return redirect('duck_code_editor:survey')
+
+    output = None
+
+    if request.method == 'POST':
+        # Check if 'Next Task' button is pressed
+        if 'next' in request.POST:
+            # If 'Next Task' button is pressed, increment the task number
 
             # Redirect to the next task with its number in the URL
-            return redirect('duck_code_editor:code_editor', task_number=next_task_number)
+            return redirect('duck_code_editor:feedback_duck', task_number=task_number)
 
         elif 'run_code' in request.POST:
             print('run_code')
@@ -55,32 +121,38 @@ def code_editor(request, task_number):
                     output = f"Error: {e}"
             else:
                 output = None
-    else: 
-        # Initialize the form with the code from the current task
-        form = CodeSnippetForm(initial={'code': current_task.get('code_to_debug', '')})
+        else:
+            form = CodeSnippetForm(
+                initial={'code': current_task.get('code_to_debug', '')})
 
+    else:
+        # Initialize the form with the code from the current task
+        form = CodeSnippetForm(
+            initial={'code': current_task.get('code_to_debug', '')})
 
     return render(request, 'duck_code_editor/code_editor.html', {
         'form': form,
         'task_number': task_number+1,
         'output': output,
         'current_task': current_task,
-        
+        'duck': True,
     })
 
-# The rest of your code remains unchanged
 
 def survey_complete(request):
     return render(request, 'duck_code_editor/survey_complete.html')
 
+
 def execute_code(code):
     try:
         # Use subprocess to run the code in a separate process
-        result = subprocess.check_output(["python", "-c", code], stderr=subprocess.STDOUT, text=True)
+        result = subprocess.check_output(
+            ["python", "-c", code], stderr=subprocess.STDOUT, text=True)
         return result.strip()
     except subprocess.CalledProcessError as e:
         # Handle errors, if any
         return f"Error: {e.output.strip()}"
+
 
 def load_tasks(json_file_path):
     try:
@@ -92,6 +164,7 @@ def load_tasks(json_file_path):
     except json.JSONDecodeError:
         return []  # Return an empty list if there's an issue decoding the JSON
 
+
 def start_experiment(request):
     if request.method == 'POST':
         selected_study_id = request.POST.get('study_id')
@@ -102,3 +175,33 @@ def start_experiment(request):
             return redirect('duck_code_editor:code_editor', task_number=0)
 
     return render(request, 'duck_code_editor/start_experiment.html')
+
+
+def break_page(request):
+    return render(request, 'duck_code_editor/break.html')
+
+
+def instructions(request):
+    return render(request, 'duck_code_editor/instructions.html')
+
+
+def rubber_duck_instructions(request):
+    return render(request, 'duck_code_editor/rubber_duck_instructions.html')
+
+
+def feedback(request, task_number):
+    if request.method == 'POST':
+        # depending on whether duck is in url as in task<nr>duck, redirect to the correct url
+        regex_pattern = re.compile(r'task\d+duck')
+        if regex_pattern.search(request.path):
+            return redirect('duck_code_editor:code_editor_duck', task_number=task_number+1)
+        else:
+            return redirect('duck_code_editor:code_editor', task_number=task_number+1)
+
+    return render(request, 'duck_code_editor/complexity_feedback.html')
+
+
+def survey(request):
+    if request.method == 'POST':
+        return redirect('duck_code_editor:survey_complete')
+    return render(request, 'duck_code_editor/survey.html')
