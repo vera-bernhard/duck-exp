17dedb2697dd82b7707aacda2715694201088de2
veralara.bernhard <veralara.bernhard@uzh.ch>
2023-11-21 17:45:20 +0100

    Collect static

--- duck_exp/settings.py
+++ duck_exp/settings.py
@@ -22,14 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = 'os.environ["SECRET_KEY"]'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'debugging-study.onrender.com',
+    '127.0.0.1'
 ]
+
 
 
 # Application definition
