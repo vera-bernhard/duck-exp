9dbf9461b8f4c9bbf4686f3492914c298d4ce26f
veralara.bernhard <veralara.bernhard@uzh.ch>
2023-11-21 15:37:31 +0100

    Update for deployment

--- duck_exp/duck_exp/settings.py
+++ duck_exp/duck_exp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +78,10 @@ WSGI_APPLICATION = "duck_exp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+	"default": dj_database_url.parse('postgres://rubbber_duck_debug_user:mCGpC2KLYGpZTsdAV2s4gAhdlxm0qGwQ@dpg-clebosnpc7cc73en6hn0-a.oregon-postgres.render.com/rubbber_duck_debug')
 }
 
 
